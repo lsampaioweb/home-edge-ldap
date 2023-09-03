5aef2110e11ade637093d0d592b11c5e6291f1ac
Luciano Sampaio Martins de Souza <lsampaioweb@gmail.com>
2023-09-02 21:13:59 -0300

    Steps to setup the backend and frontend.

---
+++ 01-Install/01-Server/files/slapd/change-password.sh
@@ -0,0 +1,10 @@
+filename="$1"
+password="$2"
+
+while read -r line
+do
+  name="$line"
+  echo "User: $name"
+  ldappasswd -H ldap://edge-ldap-01.homelab -x -D "cn=admin,dc=homelab" -W -s $password "$name"
+  echo ""
+done < "$filename"
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# home-edge-ldap
+# Home Edge LDAP
 Repository with the settings used on my OpenLDAP on the Home environment.
