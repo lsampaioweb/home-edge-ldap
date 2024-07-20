afa1e2ec721c97baaf7c7b16a0423972e76f149c
Luciano Sampaio <lsampaioweb@gmail.com>
2024-07-20 11:02:06 -0300

    refactor: Change homelab domain from *.homelab to *.lan.homelab.

--- 01-Install/01-Server/files/slapd/change-password.sh
+++ 01-Install/01-Server/files/slapd/change-password.sh
@@ -5,6 +5,6 @@ while read -r line
 do
   name="$line"
   echo "User: $name"
-  ldappasswd -H ldap://edge-ldap-01.homelab -x -D "cn=admin,dc=homelab" -W -s $password "$name"
+  ldappasswd -H ldap://edge-ldap-01.lan.homelab -x -D "cn=admin,dc=homelab" -W -s $password "$name"
   echo ""
 done < "$filename"
